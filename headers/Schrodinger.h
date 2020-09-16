e54cd17e135f996ebc7f531e218809235f9106ab
Vivien GAGLIANO <vivien.gagliano@pc233-20.pedago.ensiie.fr>
2020-09-16 12:45:03 +0200

    Completed shrodinger.h

--- headers/Schrodinger.h
+++ headers/Schrodinger.h
@@ -1,4 +1,16 @@
 #ifndef SCHRODINGER_H
 #define SCHRODINGER_H
 
+#include <armadillo>
+
+class Schrodinger {
+    public:
+        arma::cx_double solution(arma::cx_double, int);
+        bool orthonormality();
+        double energy(int);
+
+    private:
+
+};
+
 #endif SCHRODINGER_H
