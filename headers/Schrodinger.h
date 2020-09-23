2d7eb7b544a62f2f81a251426d9c0019c4ef37e3
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-09-23 12:07:27 +0200

    Added solution function in Schrodinger.h/cpp

--- headers/Schrodinger.h
+++ headers/Schrodinger.h
@@ -10,6 +10,7 @@ class Schrodinger {
         const double pi = 1;
         const double h = 1;
 
+        double solution(int, double);
         bool orthonormality();
         double energy(int);
 
