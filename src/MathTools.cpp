aa1eddd9e43b5cdb66a5cba7fb22d4b7cdeeacf1
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-09-23 12:44:10 +0200

    typo

--- src/MathTools.cpp
+++ src/MathTools.cpp
@@ -32,7 +32,7 @@ double MathTools::hermit(int n, double z) {
  * 
  * This recursive function computes the factorial of n
  * 
- * @param n int to computes the factorial of
+ * @param n integer to computes the factorial of
  */
 int MathTools::factorial(int n) {
     if (n == 0 || n == 1)
