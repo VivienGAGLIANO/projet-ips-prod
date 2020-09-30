b8199077dd8e43e0bc2fd3b19dba325bb3bf810a
Vivien GAGLIANO <vivien.gagliano@pc233-20.pedago.ensiie.fr>
2020-09-30 13:12:53 +0200

    Removed hermit function from the class

--- headers/MathTools.h
+++ headers/MathTools.h
@@ -6,11 +6,9 @@
 class MathTools {
     public:
         static double integrate(double (double));
-        static double hermit(int, double);
         static int factorial(int);
 
     private:
-        arma::vec fast_hermit(int, double);
 
     protected:
 
