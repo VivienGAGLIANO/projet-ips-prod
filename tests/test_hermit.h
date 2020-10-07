1c1bbdc815254c56cdf699afdda4cf73ad84a82a
Mathis Boiteau <mathis.boiteau@ensiie.fr>
2020-10-07 11:58:40 +0200

    Adding unit test for hermit.cpp

---
+++ tests/test_hermit.h
@@ -0,0 +1,25 @@
+#include <cxxtest/TestSuite.h>
+#include "../src/hermit.cpp"
+
+class TestHermit : public CxxTest::TestSuite
+{
+	public:
+	void testHermit(void)
+	{
+		Hermit herm(arma::rowvec("-2 0 2"));
+	       	TS_ASSERT_EQUALS(herm.get(0)(0),1);
+		TS_ASSERT_EQUALS(herm.get(0)(1),1);
+		TS_ASSERT_EQUALS(herm.get(0)(2),1);
+		TS_ASSERT_EQUALS(herm.get(1)(2),4);
+		TS_ASSERT_EQUALS(herm.get(1)(-2),-4);
+		TS_ASSERT_EQUALS(herm.get(4)(2),76);
+		TS_ASSERT_EQUALS(herm.get(4)(0),76);
+		TS_ASSERT_EQUALS(herm.get(4)(1),12);
+		TS_ASSERT_EQUALS(herm.get(5)(2),-16);
+		TS_ASSERT_EQUALS(herm.get(5)(0),16);
+		TS_ASSERT_EQUALS(herm.get(5)(1),0);
+		TS_ASSERT_EQUALS(herm.get(6)(2),-824);
+		TS_ASSERT_EQUALS(herm.get(6)(0),-824);
+		TS_ASSERT_EQUALS(herm.get(6)(1),-120);
+	}
+};
