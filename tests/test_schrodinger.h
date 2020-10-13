2717ef89f535e0f080bd315942092add115d0c66
Mathis Boiteau <mathis.boiteau@gmail.com>
2020-10-13 15:54:51 +0200

    Unit test for Schrodinger.cpp, testing Schrodinger::psi

--- tests/test_schrodinger.h
+++ tests/test_schrodinger.h
@@ -1,18 +1,41 @@
 #include <cxxtest/TestSuite.h>
 #include "../src/Schrodinger.cpp"
+#include "../src/Hermit.cpp"
+#include "../src/MathTools.cpp"
+#include <armadillo>
+#include <typeinfo>
 
 class TestSchrodinger : public CxxTest::TestSuite
 {
 	public:
-	void testSchrodinger(void)
+	void testSchrodingerPSI(void)
 	{
 		Schrodinger schro;
-	       	TS_ASSERT_EQUALS(schro.solution(5,4.5,/* JSP wallah*/));
-		TS_ASSERT_EQUALS(schro.solution(5,4.5,/* JSP wallah*/));
-		TS_ASSERT_EQUALS(schro.solution(5,4.5,/* JSP wallah*/));
-		TS_ASSERT_EQUALS(schro.solution(5,4.5,/* JSP wallah*/));
-		TS_ASSERT_EQUALS(schro.solution(5,4.5,/* JSP wallah*/));
-		TS_ASSERT_EQUALS(schro.solution(5,4.5,/* JSP wallah*/));
-		TS_ASSERT_EQUALS(schro.solution(5,4.5,/* JSP wallah*/));
+		Hermit herm(arma::colvec({6,4,9,3,0,-1,-2}));
+		arma::colvec value = arma::exp(arma::colvec({-18})) * arma::sqrt(arma::colvec({6})) * herm.get(4)(0) / 48;
+		TS_ASSERT_DELTA(schro.psi(4,{6})(0), value(0) , 0.000000000001);
+		value = arma::exp(arma::colvec({-8}))* herm.get(0)(1);
+		TS_ASSERT_DELTA(schro.psi(0,{4})(0),value(0),0.000000000001);
+		value = arma::exp(arma::colvec({-81/2})) * arma::sqrt(arma::colvec({3})) * herm.get(3)(2) / 12;
+		TS_ASSERT_DELTA(schro.psi(3,{9})(0),value(0),0.000000000001);
+		value = arma::exp(arma::colvec({-9/2})) * arma::sqrt(arma::colvec({2})) * herm.get(1)(3) / 2;
+		TS_ASSERT_DELTA(schro.psi(1,{3})(0),value(0),0.000000000001);
+		value = arma::sqrt(arma::colvec({15})) * herm.get(5)(4) / 240;
+		TS_ASSERT_DELTA(schro.psi(5,{0})(0),value(0),0.000000000001);
+		value = arma::exp(arma::colvec({-1/2})) * arma::sqrt(arma::colvec({2})) * herm.get(1)(5) / 2;
+		TS_ASSERT_DELTA(schro.psi(1,{-1})(0),value(0),0.000000000001);
+		value = arma::exp(arma::colvec({-2})) * arma::sqrt(arma::colvec({6})) * herm.get(4)(6) / 48;
+		TS_ASSERT_DELTA(schro.psi(4,{-2})(0),value(0),0.000000000001);
+	}
+	void testSchrodingerPSI_SECOND(void)
+	{
+		Schrodinger schro;
+	    /*TS_ASSERT_EQUALS(schro.psi_second(5,{4.5,7,8}),0);*/
+	}
+
+	void testSchrodingerPSI_ENERGY(void)
+	{
+		Schrodinger schro;
+	    /*TS_ASSERT_EQUALS(schro.psi_second(5,{4.5,7,8}),0);*/
 	}
 };
