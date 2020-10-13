b83d1a9114aae7a84d78f5600aa2722dd5921b7d
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-10-13 18:55:27 +0200

    Updated main() to generate psi.csv with 1000 values for psi(z)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 #include "headers/Schrodinger.h"
 
 int main() {
-    // std::cout << "Hello World!";
+    int start = -5;
+    int end = 5;
+    int N = 1000;
+
+    Schrodinger schro;
+    arma::vec z = arma::linspace(start, end, N);
+    arma::vec psi_z = schro.psi(1, z);
+
+    std::ofstream csv_file;
+    csv_file.open("psi.csv");
+    csv_file << "z,psi(z)" << std::endl;
+    for (int i = 0; i < N; i++) {
+        csv_file << z(i) << ',' << psi_z(i) << std::endl;
+    }
+    csv_file.close();
+
     return 0;
 }
