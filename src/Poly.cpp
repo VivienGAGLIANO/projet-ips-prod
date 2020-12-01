23d3d003357b160c9d966985df0302dc5fb528c1
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-12-01 16:21:08 +0100

    Finished Poly class, with Laguerre polynomials calculation, but tests for Laguerre do not pass

--- src/Poly.cpp
+++ src/Poly.cpp
@@ -18,7 +18,7 @@ void Poly::calcHermite(int n, arma::vec mesh) {
     if (n < 0) {
         throw "Invalid polynomial index. Positive integers only.";
     }
-    mesh_hermite = mesh;
+    this->mesh = mesh;
 //    if (!arma::approx_equal(mesh_hermite, mesh, "absdiff", 1E-10)){
 //        mesh_hermite = mesh;
 //        hermite_values = arma::mat(mesh_hermite.n_elem, 1).ones();
@@ -45,22 +45,25 @@ void Poly::calcHermite(int n, arma::vec mesh) {
  * @param n rank n of the desired laguerre polynomial
  * @param m rank m of the desired laguerre polynomial
  */
-void Poly::calcLaguerre(int m, int n, arma::vec mesh) {
-    if (n < 0 || m < 0) {
+void Poly::calcLaguerre(int m_max, int n_max, arma::vec mesh) {
+    if (n_max < 0 || m_max < 0) {
         throw "Invalid polynomial index. Positive integers only.";
     }
-    mesh_laguerre = mesh;
-    m_laguerre = m;
-    laguerre_values = arma::mat(mesh.n_elem, 1).ones();
-    int p = laguerre_values.n_cols;
-    while (p <= n) {
-        arma::colvec next_col;
-        if (p == 1)
-            next_col = laguerre_values.col(0) + m - mesh;
-        else
-            next_col = (2 + (m-1-mesh)/p) % laguerre_values.col(p-1) - (1 + (m - 1)/p) * laguerre_values.col(p-2);
-        laguerre_values.insert_cols(p, next_col);
-        p++;
+    this->mesh = mesh;
+    laguerre_values = arma::cube(mesh.n_elem, n_max + 1, m_max + 1);
+    for (int m = 0; m <= m_max; m++) {
+        arma::mat current_slice = arma::mat(mesh.n_elem, 1).ones();
+        int n = current_slice.n_cols;
+        while (n <= n_max) {
+            arma::colvec next_col;
+            if (n == 1)
+                next_col = 1 + m - mesh;
+            else
+                next_col = (2+(m-1-mesh)/n) % current_slice.col(n-1) - (1 + (m-1)/n) * current_slice.col(n-2);
+            current_slice.insert_cols(n, next_col);
+            n++;
+        }
+        laguerre_values.slice(m) = current_slice;
     }
 }
 
@@ -90,30 +93,8 @@ arma::vec Poly::hermite(int n) {
  * @deprecated
  */
 arma::vec Poly::laguerre(int m, int n) {
-    if (n >= (int)laguerre_values.n_cols) {
-        throw "Values not computed for inputed integer. Run calcLaguerre first.\n";
+    if (n >= (int)laguerre_values.n_cols || m >= (int)laguerre_values.n_slices) {
+        throw "Values not computed for inputed integers. Run calcLaguerre first.\n";
     }
-    if (m != m_laguerre) {
-        calcLaguerre(m, std::max(n, (int) laguerre_values.n_cols - 1), mesh_laguerre);
-    }
-    return laguerre_values.col(n);
-}
-
-
-/**
- * << operator to print hermit object on stdout stream
- *
- * Prints out the hermite_values matrix, showing all computed ranks of polynomials, along the object's mesh_hermite. Use Poly::insert_size to set matrix cells size.
- * @param stream stream on which to add hermit print
- * @param hermit object to print
- * @return stream with hermite_values matrix printed on it
- */
-std::ostream& operator<<(std::ostream &stream, const Poly &hermit) {
-    for (int z = 0; z < (int) hermit.hermite_values.n_cols; ++z) {
-        for (int n = 0; n < (int) hermit.hermite_values.n_rows; ++n) {
-            stream /*<< std::setfill('.')*/ << std::setw(hermit.insert_size) << hermit.hermite_values(n, z) << " ";
-        }
-        stream << std::endl;
-    }
-    return stream;
+    return laguerre_values.slice(m).col(n);
 }
