cf8cefae5598ecc98f79ae26c94592bc6833d9e1
Vivien GAGLIANO <gagliano.vivien@gmail.com>
2020-12-15 19:48:51 +0100

    Optimization step 6 : not complete. Code in comment.

--- src/OptimizedRho.cpp
+++ src/OptimizedRho.cpp
@@ -20,14 +20,14 @@ arma::mat OptimizedRho::density(arma::vec zVals, arma::vec rVals) {
     rho.load("rho.arma", arma::arma_ascii);
     arma::mat result = arma::zeros(zVals.size(), rVals.size()); // number of points on r- and z- axes
     Basis basis(1.935801664793151, 2.829683956491218, 14, 1.3);
-
+/*rPart_b.t()*/
     int i = 0;
     arma::mat zParts(zVals.n_elem, basis.n_zMax(0, 0));
-    //arma::cube rParts(rVals.n_elem, basis.mMax, basis.nMax(0));
+//    arma::cube rParts(rVals.n_elem, basis.mMax, basis.nMax(0));
     arma::icube index(basis.mMax, basis.nMax(0), basis.n_zMax(0, 0), arma::fill::zeros);
     for (int m = 0; m < basis.mMax; ++m) {
         for (int n = 0; n < basis.nMax(m); ++n) {
-            //rParts.col(m, n) = ;
+//            rParts.slice(n).col(m) = basis.rPart(rVals, m, n);
             for (int n_z = 0; n_z < basis.n_zMax(m, n); ++n_z) {
                 if (m == 0 && n == 0) {
                     zParts.col(n_z) = basis.zPart(zVals, n_z);
@@ -51,8 +51,8 @@ arma::mat OptimizedRho::density(arma::vec zVals, arma::vec rVals) {
                     {
                         rPart_b = basis.rPart(rVals, m_a, n_b);
                         for (int n_z_b = 0; n_z_b < basis.n_zMax(m_a, n_b); n_z_b++) {
-                            arma::mat funcA = zParts.col(n_z_a) * rPart_a.t();
-                            arma::mat funcB = zParts.col(n_z_b) * rPart_b.t();
+                            arma::mat funcA = zParts.col(n_z_a) * rPart_a.t() /*rParts.slice(n_a).col(m_a)*/;
+                            arma::mat funcB = zParts.col(n_z_b) * rPart_b.t() /*rParts.slice(n_b).col(m_a)*/;
                             result += funcA % funcB * rho(index(m_a, n_a, n_z_a), index(m_a, n_b, n_z_b));
                         }
                     }
