340307101ee52fcc79fb6e29d12732aa8456a117
Lilian MALLARDEAU <lilian.mallardeau@ensiie.fr>
2020-12-15 19:18:15 +0100

    Finished coordinates conversion and python script to generate 2d plot

--- generate_2d_plot.py
+++ generate_2d_plot.py
@@ -4,11 +4,17 @@ from matplotlib import pyplot
 
 density = numpy.loadtxt(open("2dplot.csv", "rb"), delimiter=",")
 
-pyplot.matshow(density)
-#pyplot.pcolormesh(density)
+pyplot.matshow(density, extent = [-20, 20, -10, 10])
+# Uncomment following line to change colors to red/orange/yellow/black
+#pyplot.set_cmap('hot')
+
+pyplot.gca().xaxis.tick_bottom()
 pyplot.xlabel("z [fm]")
 pyplot.ylabel("x [fm]")
-pyplot.axis("off")
-#pyplot.xlim(-20, 20)
-#pyplot.ylim(-10, 10)
+
+bar = pyplot.colorbar()
+bar.set_label("density")
+
+pyplot.title("Nuclear density as a function of x and z")
+
 pyplot.savefig("2dplot.png")
--- main.cpp
+++ main.cpp
@@ -33,36 +33,30 @@ std::string cubeToDf3(const arma::cube &m)
     return ss.str();
 }
 
-arma::cube convert_coordinates(arma::mat input, int r_max) {
+arma::cube convert_coordinates(arma::mat input) {
     int nbp_r = input.n_cols;
     int nbp_z = input.n_rows;
-    int x_min = -r_max, x_max = r_max, nbp_x = 32;
-    int y_min = -r_max, y_max = r_max, nbp_y = 32;
+    int x_min = -10, x_max = 10, nbp_x = 32;
+    int y_min = -10, y_max = 10, nbp_y = 32;
 
     arma::cube output = arma::cube(nbp_x, nbp_y, nbp_z).zeros();
     for (int x = 0; x < nbp_x; x++) {
         for (int y = 0; y < nbp_y; y++) {
-            for (int z = 0; z < nbp_z; z++) {
-//                double real_x = x_min + x * ((x_max - x_min)/nbp_x);
-//                double real_y = y_min + y * ((y_max - y_min)/nbp_y);
-                double real_x = x_min + (x / x_max) * (x_min / x_max);
-                double real_y = y_min + (y / y_max) * (y_min / y_max);
-                int r = sqrt(pow(real_x, 2) + pow(real_y, 2));
-                if (r >= nbp_r)
-                    output(x, y, z) = 0;
-                else
-                    output(x, y, z) = output(x, y, z) = input(z, r);
-            }
+            double real_x = x_min + ((double) x/nbp_x)*(x_max - x_min);
+            double real_y = y_min + ((double) y/nbp_y)*(y_max - y_min);
+            double real_r = sqrt(pow(real_x, 2) + pow(real_y, 2));
+            int r = (real_r/sqrt(x_max*x_max + y_max*y_max)) * nbp_r;
+            if (r < nbp_r)
+                output.tube(x, y) = input.col(r);
         }
     }
     return output;
 }
 
-
 int main() {
     arma::vec zVals = arma::linspace(-20, 20, 64);
     arma::vec rVals = arma::linspace(0, 10, 16);
-    arma::cube out = convert_coordinates(OptimizedRho::density(zVals, rVals), 10);
+    arma::cube out = convert_coordinates(OptimizedRho::density(zVals, rVals));
 
     arma::mat flatview = out.subcube(0, out.n_cols/2, 0, out.n_rows - 1, out.n_cols/2, out.n_slices - 1);
     flatview.save("2dplot.csv", arma::csv_ascii);
